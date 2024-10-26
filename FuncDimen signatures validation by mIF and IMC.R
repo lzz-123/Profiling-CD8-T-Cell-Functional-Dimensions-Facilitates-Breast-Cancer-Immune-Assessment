9b9fc8c096095c63b7c2911365e23d68fb1518c8
lzz-123 <105848066+lzz-123@users.noreply.github.com>
2024-10-26 16:51:57 +0800

    FuncDimen signatures validation by multiplex immunofluorescence and imaging mass cytometry.R

--- FuncDimen signatures validation by mIF and IMC.R
+++ FuncDimen signatures validation by mIF and IMC.R
@@ -1,16 +1,17 @@
-#mIF based functional dimension evaluation (counted by pathologists)
+#multiplex immunofluorescence  based functional dimension evaluation (counted by pathologists)
 
-#IMC based functional dimension evaluation(protein)
-#CD8+ T cell determination (cutoff determination)
+#imaging mass cytometry based functional dimension evaluation(protein)
+#determination of the cut off values for CD8 positive cells
 #sce1 is the processed single cell matrix (protein)
+#the cutoff value used for determination of CD8 positive cell is 0.35
 library(CATALYST)
 sce1 <- estCutoffs(sce1)
 metadata(sce1)$sep_cutoffs
-#CD8 matrix derived
 sce1 <- AddMetaData(sce1, metadata = FetchData(sce1, vars = "CD8a"), col.name = "CD8A")
 sce1a<-subset(sce1,subset =CD8A >0.35)
 
-#specific markers cut off determination
+#determination of the cut off values for GZMB, MKI67 and CASP3 positive cells
+#the cutoff values used for determination of GZMB, MKI67 and CASP3 positive cell are 0.85, 0.15, and 0.05, respectively
 sce1a <- estCutoffs(sce1a)
 metadata(sce1a)$sep_cutoffs
 sce1a <- AddMetaData(sce1a, metadata = FetchData(sce1a, vars = "Granzyme_B"), col.name = "GB")
@@ -21,12 +22,11 @@ meta1$GB<-ifelse(meta1$GB>0.85,1,0)
 meta1$Ki67<-ifelse(meta1$Ki67>0.15,1,0)
 meta1$CAPS3<-ifelse(meta1$CAPS3>0.05,1,0)
 
-#percentages of CD8+ T cell expressing specific marker
+#abundance of CD8+ T cell expressing specific marker
 #total is the total cell counts for each participant
 assignment1<-aggregate(meta1$GB,by=list(meta1$orig.ident),FUN=sum,na.rm=TRUE)
 assignment2<-aggregate(meta1$Ki67,by=list(meta1$orig.ident),FUN=sum,na.rm=TRUE)
 assignment3<-aggregate(meta1$CAPS3,by=list(meta1$orig.ident),FUN=sum,na.rm=TRUE)
-
 total<-left_join(total,assignment1,by="orig.ident")
 total<-left_join(total,assignment2,by="orig.ident")
 total<-left_join(total,assignment3,by="orig.ident")
@@ -34,16 +34,17 @@ total$cytotoxicity<-total$GB/total$total
 total$proliferation<-total$Ki67/total$total
 total$apoptosis<-total$CAPS3/total$total
 
-#IMC based functional dimension evaluation(RNAscope)
-#CD8+ T cell determination (cutoff determination)
+#imaging mass cytometry based functional dimension evaluation(RNAscope)
+#determination of the cut off values for CD8 positive cells
 #sce2 is the processed single cell matrix (RNAscope)
+#the cutoff value used for determination of CD8 positive cell is 0.36
 sce2 <- estCutoffs(sce2)
 metadata(sce2)$sep_cutoffs
-#CD8 matrix derived
 sce2 <- AddMetaData(sce2, metadata = FetchData(sce1, vars = "CD8a"), col.name = "CD8A")
 sce2a<-subset(sce2,subset =CD8A >0.6)
 
-#specific markers cut off determination
+#determination of the cut off values for CXCL13 and CXCL9 positive cells
+#the cutoff values used for determination of CXCL13 and CXCL9 positive cell are 1.2 and 0.25, respectively
 sce2a <- estCutoffs(sce2a)
 metadata(sce2a)$sep_cutoffs
 sce2a <- AddMetaData(sce2a, metadata = FetchData(sce2a, vars = "CXCL13"), col.name = "CXCL13")
@@ -52,11 +53,10 @@ meta2<-sce2a@meta.data
 meta2$CXCL13<-ifelse(meta2$CXCL13>1.2,1,0)
 meta2$CXCL9<-ifelse(meta2$CXCL9>0.25,1,0)
 
-#percentages of CD8+ T cell expressing specific marker
+#abundance of CD8+ T cell expressing specific marker
 #total is the total cell counts for each participant
 assignment1<-aggregate(meta2$CXCL13,by=list(meta2$orig.ident),FUN=sum,na.rm=TRUE)
 assignment2<-aggregate(meta2$CXCL9,by=list(meta2$orig.ident),FUN=sum,na.rm=TRUE)
-
 total<-left_join(total,assignment1,by="orig.ident")
 total<-left_join(total,assignment2,by="orig.ident")
 total$reactivity<-total$CXCL13/total$total
